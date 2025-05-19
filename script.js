636ce73d36d2ac7165cb22566547af83f6616237
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-05-19 16:56:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+const name = document.getElementById("text").value.trim();
+const delay = Number(document.getElementById("delay").value); // Convert to a number
+
+const promise1 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(name);
+        }, delay * 1000);
+    });
+}
+
+document.getElementById('btn').addEventListener("click", async () => {
+    try {
+        const data = await promise1();
+        document.getElementById('output').textContent = data;
+        document.getElementById('output').style.color = "black";
+    } catch (err) {
+        document.getElementById('output').textContent = err;
+    }
+});
+	
+});
